bb43e7d904d1c0db8beaabd9372d969c4bfd8fc8
mokunshao <mokunshao@foxmail.com>
2020-02-05 18:08:16 +0800

    Update generator_expression.py

--- generator_expression.py
+++ generator_expression.py
@@ -3,13 +3,22 @@ letters = (item for item in 'abc')
 for item in letters:
     print(item)
 
+print('---')
+
 letters2 = (item*2 for item in [1, 3, 4])
 
 for item in letters2:
     print(item)
 
-letters3 = (item*2 for item in [11, 33, 44])
+print('---')
 
+letters3 = (item*2 for item in range(0, 10))
+
+print(next(letters3))
+print(next(letters3))
+print(next(letters3))
+print(next(letters3))
+print(next(letters3))
 print(next(letters3))
 print(next(letters3))
 print(next(letters3))
